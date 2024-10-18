80ffbfac0fa575ed1464eb06e005bf906cd68615
Hope <hopemwaura39@gmail.com>
2024-10-18 13:49:22 +0300

    initial commit

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
-SQLALCHEMY_DATABASE_URI = 'sqlite:///school_system.db'
+import os
+
+SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "postgresql://schoolsystemdatabase_user:XMIC0d70sPEmLgPK9ME2U7WmsNeHN8Pp@dpg-cs93bttds78s73c84h3g-a.oregon-postgres.render.com/schoolsystemdatabase")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = 'your-secret-key'  # Change this to a secure random key
 JWT_SECRET_KEY = 'your-jwt-secret-key'  # Change this to a different secure random key
+
+# postgresql://schoolsystemdatabase_user:XMIC0d70sPEmLgPK9ME2U7WmsNeHN8Pp@dpg-cs93bttds78s73c84h3g-a.oregon-postgres.render.com/schoolsystemdatabase
