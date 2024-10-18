45329d6c611a4d317636d2df8a9d9e0de684456f
Hope <hopemwaura39@gmail.com>
2024-10-18 14:18:44 +0300

    changes

--- myapp/app.py
+++ myapp/app.py
@@ -4,48 +4,25 @@ from flask_jwt_extended import JWTManager
 from myapp.models import db, User
 from myapp.resources import api
 
-def create_app():
-    app = Flask(__name__)
 
-    app.config.from_pyfile('config.py')
+app = Flask(__name__)
 
-    db.init_app(app)
-    
-    migrate = Migrate(app, db)
-
-    jwt = JWTManager(app)
-
-    @jwt.user_identity_loader
-    def user_identity_lookup(user):
-        return user.id
-
-    @jwt.additional_claims_loader
-    def add_claims_to_access_token(user):
-        return {'role': user.role}
-
-    api.init_app(app)
-    
-    if __name__ == '__main__':
-        app.run(debug=True)
-
-# app = Flask(__name__)
-
-# app.config.from_pyfile('config.py')
+app.config.from_pyfile('config.py')
 
 
-# db.init_app(app)
-# migrate = Migrate(app, db)
+db.init_app(app)
+migrate = Migrate(app, db)
 
-# jwt = JWTManager(app)
+jwt = JWTManager(app)
 
-# @jwt.user_identity_loader
-# def user_identity_lookup(user):
-#     return user.id
+@jwt.user_identity_loader
+def user_identity_lookup(user):
+    return user.id
 
-# @jwt.additional_claims_loader
-# def add_claims_to_access_token(user):
-#     return {'role': user.role}
+@jwt.additional_claims_loader
+def add_claims_to_access_token(user):
+    return {'role': user.role}
 
-# api.init_app(app)
+api.init_app(app)
 
 
